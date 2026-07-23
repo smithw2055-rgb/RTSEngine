6a13b2d2401430d4630a5d2b0b7e8e34ce28d9e8
Max <smith.w2055@gmail.com>
2026-07-23 18:06:06 +0800

    feat(roguelite): record reward rarity outcomes in run history

--- framework/roguelite/include/RTSEngine/Roguelite/RunHistory.h
+++ framework/roguelite/include/RTSEngine/Roguelite/RunHistory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <RTSEngine/Roguelite/ModifierRuntime.h>
+#include <RTSEngine/Roguelite/RewardRarity.h>
 #include <RTSEngine/TowerDefense/WaveDirector.h>
 
 #include <cstdint>
@@ -47,6 +48,15 @@ struct WaveResult final {
     ModifierId selectedModifier{};
     bool modifierApplied{};
 
+    std::uint32_t rewardRarityBudget{};
+    std::uint32_t rewardRaritySpent{};
+    RewardRarity guaranteedRarity{RewardRarity::Common};
+    RewardRarity effectiveGuaranteedRarity{RewardRarity::Common};
+    std::uint32_t pityBefore{};
+    std::uint32_t pityAfter{};
+    bool pityTriggered{};
+    std::vector<RewardRarity> rewardRarities;
+
     friend bool operator==(const WaveResult& a,
                            const WaveResult& b) noexcept {
         return a.waveId == b.waveId &&
@@ -69,7 +79,16 @@ struct WaveResult final {
                a.bosses == b.bosses &&
                a.rewardChoices == b.rewardChoices &&
                a.selectedModifier == b.selectedModifier &&
-               a.modifierApplied == b.modifierApplied;
+               a.modifierApplied == b.modifierApplied &&
+               a.rewardRarityBudget == b.rewardRarityBudget &&
+               a.rewardRaritySpent == b.rewardRaritySpent &&
+               a.guaranteedRarity == b.guaranteedRarity &&
+               a.effectiveGuaranteedRarity ==
+                   b.effectiveGuaranteedRarity &&
+               a.pityBefore == b.pityBefore &&
+               a.pityAfter == b.pityAfter &&
+               a.pityTriggered == b.pityTriggered &&
+               a.rewardRarities == b.rewardRarities;
     }
 };
 
@@ -79,6 +98,7 @@ struct RunHistory final {
     std::uint64_t finishedTick{};
     RunHistoryPhase phase{RunHistoryPhase::Idle};
     bool legacyImported{};
+    std::uint32_t rewardPityMisses{};
     std::vector<WaveResult> waves;
 
     friend bool operator==(const RunHistory& a,
@@ -88,6 +108,7 @@ struct RunHistory final {
                a.finishedTick == b.finishedTick &&
                a.phase == b.phase &&
                a.legacyImported == b.legacyImported &&
+               a.rewardPityMisses == b.rewardPityMisses &&
                a.waves == b.waves;
     }
 };
