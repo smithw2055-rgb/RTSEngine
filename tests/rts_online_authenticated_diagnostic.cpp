d261ae03c67f2432a08541813fdcd8d6c93b0aac
Max <smith.w2055@gmail.com>
2026-07-28 07:58:37 +0800

    Use shared online assertion diagnostics

--- tests/rts_online_authenticated_diagnostic.cpp
+++ tests/rts_online_authenticated_diagnostic.cpp
@@ -1,18 +1,6 @@
-#include <cstdio>
-#include <cstdlib>
-
-namespace std {
-[[noreturn]] inline void rtsDiagnosticAbort(int line) {
-    std::fprintf(stderr, "authenticated online assertion failed at line %d\n", line);
-    std::_Exit(134);
-}
-} // namespace std
-
-#define abort() rtsDiagnosticAbort(__LINE__)
 #define main rts_online_hardening_combined_main
 #include "rts_online_hardening_tests.cpp"
 #undef main
-#undef abort
 
 int main() {
     testAuthenticatedEncryptedRuntimeAndMigration();
