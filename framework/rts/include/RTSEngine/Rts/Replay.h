d96c37c7186085ad67413faed04a9f51afabc09a
Max <smith.w2055@gmail.com>
2026-07-27 21:36:08 +0800

    Allow Gather commands in RTS replay archives

--- framework/rts/include/RTSEngine/Rts/Replay.h
+++ framework/rts/include/RTSEngine/Rts/Replay.h
@@ -193,7 +193,7 @@ inline bool ReadTickCommand(
         !reader.readU32(command.targetEntity.generation)) {
         return false;
     }
-    if (rawType > static_cast<std::uint8_t>(CommandType::HoldPosition)) {
+    if (rawType > static_cast<std::uint8_t>(CommandType::Gather)) {
         return false;
     }
     command.type = static_cast<CommandType>(rawType);
