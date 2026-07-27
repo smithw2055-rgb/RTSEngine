23e5188909b66c4c2affa7d7c5e30b805d7844d3
Max <smith.w2055@gmail.com>
2026-07-27 16:08:10 +0800

    feat(rts): finalize complete authoritative world hash

---
+++ framework/rts/include/RTSEngine/Rts/AuthoritativeStateHash.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <RTSEngine/Rts/Navigation.h>
+#include <rts/foundation/CanonicalHash.h>
+
+#include <cstdint>
+
+namespace rts::gameplay {
+
+inline std::uint64_t FinalizeRtsAuthoritativeWorldHash(
+    std::uint64_t baseWorldHash,
+    std::uint64_t entityRegistryHash,
+    GridPoint requiredPathStart,
+    GridPoint requiredPathGoal,
+    std::uint32_t nextConstructionId,
+    std::uint32_t nextProductionId,
+    std::uint32_t playerTeamId,
+    std::uint16_t compatibilityVersion = 3u) noexcept {
+    if (compatibilityVersion < 3u) return baseWorldHash;
+
+    foundation::CanonicalHash hash;
+    hash.WriteString("rts.authoritative-world.v3");
+    hash.WriteU64(baseWorldHash);
+    hash.WriteU64(entityRegistryHash);
+    hash.WriteI32(requiredPathStart.x);
+    hash.WriteI32(requiredPathStart.y);
+    hash.WriteI32(requiredPathGoal.x);
+    hash.WriteI32(requiredPathGoal.y);
+    hash.WriteU32(nextConstructionId);
+    hash.WriteU32(nextProductionId);
+    hash.WriteU32(playerTeamId);
+    return hash.Value();
+}
+
+} // namespace rts::gameplay
