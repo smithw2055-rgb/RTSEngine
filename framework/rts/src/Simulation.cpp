1f9c83689f02be13041df064815035549754164d
Max <smith.w2055@gmail.com>
2026-07-27 16:08:33 +0800

    feat(rts): finalize authoritative hash after snapshot

--- framework/rts/src/Simulation.cpp
+++ framework/rts/src/Simulation.cpp
@@ -1,3 +1,4 @@
+#include <RTSEngine/Rts/AuthoritativeStateHash.h>
 #include <RTSEngine/Rts/Simulation.h>
 
 namespace rts::gameplay {
@@ -10,6 +11,21 @@ RtsSimulation::RtsSimulation(std::int32_t width, std::int32_t height)
       building_(resources_, navigation_),
       combat_(width, height) {
     installSystems();
+    scheduler_.add(
+        ecs::Stage::Snapshot,
+        10,
+        410,
+        [this](ecs::World& world, const ecs::SystemContext&) {
+            snapshot_.worldHash = FinalizeRtsAuthoritativeWorldHash(
+                snapshot_.worldHash,
+                world.entityRegistryHash(),
+                requiredPathStart_,
+                requiredPathGoal_,
+                building_.nextConstructionId(),
+                nextProductionId_,
+                playerTeamId_);
+            influenceWorldHash_ = snapshot_.worldHash;
+        });
 }
 
 RtsSimulation::~RtsSimulation() = default;
