fc915c5d1ade0a216404b3c9ed0f478d84fbc0f5
Max <smith.w2055@gmail.com>
2026-07-28 08:10:23 +0800

    Remove temporary dedicated diagnostic target

--- tests/rts_online_dedicated_diagnostic.cpp
+++
@@ -1,8 +0,0 @@
-#define main rts_online_hardening_combined_main
-#include "rts_online_hardening_tests.cpp"
-#undef main
-
-int main() {
-    testDedicatedServerHasNoLocalPlayer();
-    return EXIT_SUCCESS;
-}
