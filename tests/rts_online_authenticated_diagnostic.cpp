9cf564d0feadee63da419f8f8ebda4a8ecdc6f12
Max <smith.w2055@gmail.com>
2026-07-28 08:10:08 +0800

    Remove temporary authenticated diagnostic target

--- tests/rts_online_authenticated_diagnostic.cpp
+++
@@ -1,8 +0,0 @@
-#define main rts_online_hardening_combined_main
-#include "rts_online_hardening_tests.cpp"
-#undef main
-
-int main() {
-    testAuthenticatedEncryptedRuntimeAndMigration();
-    return EXIT_SUCCESS;
-}
